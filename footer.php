4fb0f307489f3586d45db0a746519b0ce8b2e299
vito <v.gaio@druckereilandquart.ch>
2018-08-31 14:20:59 +0200

    kontakt-link fixed/responsive

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
     <div class="kontakt">
-        <p>Gabriela Capeder Heckner &nbsp|  &nbspObere Pardellen 6 &nbsp| &nbsp7304 Maienfeld &nbsp| &nbspM +41 79 446 96 17&nbsp | &nbsp <a href="mailto:info@sendalva.ch"info@sendalva.ch>info@sendalva.ch</a></p>
+        <p>Gabriela Capeder Heckner &nbsp|  &nbspObere Pardellen 6 &nbsp| &nbsp7304 Maienfeld &nbsp| &nbspM +41 79 446 96 17&nbsp | &nbsp <a href="mailto:info@sendalva.ch">info@sendalva.ch</a></p>
     </div>
 </footer>
